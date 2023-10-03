fcb59e273386a6294a7799107b24a51255443701
Alexandre Philibert <alexandre.philibert@ik.me>
2023-10-03 13:06:54 +0200

    Setup picocli command

--- src/main/java/ch/kodai/templ8/Main.java
+++ src/main/java/ch/kodai/templ8/Main.java
@@ -1,7 +1,19 @@
 package ch.kodai.templ8;
 
-public class Main {
-    public static void main(String[] args) {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
+import java.util.concurrent.Callable;
+
+@Command(name = "templ8", mixinStandardHelpOptions = true, version = "0.0.1", description = "A Kubernetes templating utility")
+public class Main implements Callable<Integer> {
+    @Override
+    public Integer call() {
+        return 0;
+    }
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
     }
 }
