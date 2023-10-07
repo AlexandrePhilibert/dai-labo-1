07e3f313bafb095511719a93e339f30f654aff95
valentin-ricard <valentin.ricard@praxo.ch>
2023-10-07 20:20:51 +0200

    feat: Added beautiful error messages

--- src/main/java/ch/kodai/templ8/values/exceptions/ValuesLoadException.java
+++ src/main/java/ch/kodai/templ8/values/exceptions/ValuesLoadException.java
@@ -2,7 +2,15 @@ package ch.kodai.templ8.values.exceptions;
 
 public class ValuesLoadException extends RuntimeException {
 
+    private final String message;
+
     public ValuesLoadException(String message) {
         super("An error occurred while parsing the values file: " + message);
+        this.message = message;
+    }
+
+
+    public String getInnerMessage() {
+        return message;
     }
 }
