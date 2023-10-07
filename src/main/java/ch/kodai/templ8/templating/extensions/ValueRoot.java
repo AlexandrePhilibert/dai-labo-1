91731e73b489527bb8496fffec06b8a1be8193a5
valentin-ricard <valentin.ricard@praxo.ch>
2023-10-07 14:56:54 +0200

    feat: Added CLI values provider

--- src/main/java/ch/kodai/templ8/templating/extensions/ValueRoot.java
+++ src/main/java/ch/kodai/templ8/templating/extensions/ValueRoot.java
@@ -2,6 +2,12 @@ package ch.kodai.templ8.templating.extensions;
 
 import ch.kodai.templ8.values.ValuesProvider;
 
+/**
+ * A utility class, used for translating pebble's access path to the one in use in the CLI.
+ * We cannot use children directly, as the console implementation is based on a single map.
+ * <p>
+ * A future interesting way to do it would be to parse and merge YAML elements, but that is for the future™
+ */
 public class ValueRoot {
     private final ValuesProvider provider;
     private final String path;
--- src/main/java/ch/kodai/templ8/templating/extensions/ValuesProviderExtension.java
+++ src/main/java/ch/kodai/templ8/templating/extensions/ValuesProviderExtension.java
@@ -8,7 +8,18 @@ import io.pebbletemplates.pebble.template.EvaluationContextImpl;
 
 import java.util.List;
 
+/**
+ * This class implements a Pebble extension.
+ * Pebble is the templating system in use for the following reasons:
+ * <ul>
+ *     <li>Its syntax is readable, and near the one in the go standard library, facilitating migration</li>
+ *     <li>It is really extensible, allowing for custom elements traversal, among other modifications</li>
+ * </ul>
+ */
 public class ValuesProviderExtension extends AbstractExtension {
+    /**
+     * Attribute resolver for our {@link ValueRoot} element.
+     */
     public static class ValueAttributeResolver implements AttributeResolver {
 
         @Override
@@ -21,7 +32,17 @@ public class ValuesProviderExtension extends AbstractExtension {
                 String filename,
                 int lineNumber) {
             if (instance instanceof ValueRoot root) {
-                return new ResolvedAttribute(root.get((String) attributeNameValue));
+                // You may be wondering why we need a custom attribute resolver instead of a map or any other solution.
+                // This is mainly due to Pebble's use of reflection, that tries to access the (get)?`attributeNameValue`
+                // function, that we cannot reasonably implement dynamically
+                if (attributeNameValue instanceof String stringName) {
+                    return new ResolvedAttribute(root.get(stringName));
+                } else {
+                    // TODO: Support arrays
+                    //  This is more than doable, but needs a more performant parser-combinator than the current
+                    //  solution, that is basically iterate over the dots.
+                    throw new RuntimeException("Arrays are not supported in this version");
+                }
             }
             return null;
         }
